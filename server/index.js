ed939a14ec35523c608d60252bbc3a38ac5f1200
Divyanshu Grover <142349410+DivyanshuGrover29@users.noreply.github.com>
2024-09-09 01:02:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-  origin: '*', // This will allow requests from any origin
+  origin: "https://career-nexus.vercel.app", // This will allow requests from any origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
